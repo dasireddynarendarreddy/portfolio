78dfbceedcb01dc97da38c5f0a051294782397c7
dasireddynarendarreddy <narendardasireddy4@gmail.com>
2024-08-10 00:39:24 +0530

    portfolio

--- pages/About.js
+++ pages/About.js
@@ -1,8 +1,10 @@
 // components/AnimatedComponent.js
 import { motion } from 'framer-motion';
+import 'animate.css/animate.min.css'
 import WavingHandIcon from '@mui/icons-material/WavingHand';
 const About= () => {
   return (
+    <div>
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
@@ -21,6 +23,19 @@ const About= () => {
       </div>
      
     </motion.div>
+    <motion.div
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    transition={{ duration: 1 }}
+    style={{ padding: '20px', textAlign: 'center' }}
+    
+  >
+       <div className="animate__heartBeat text-8xl">
+       Hi, I’m Narendar, a recent B.Tech graduate specializing in Computer Science.
+       I graduated with a Bachelor’s degree from Anurag College in 2024 june.
+       </div>
+    </motion.div>
+    </div>
   );
 };
 
