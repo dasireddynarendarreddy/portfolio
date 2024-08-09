7d37b974dfef6a89dd3a24701915db94f27667b0
dasireddynarendarreddy <narendardasireddy4@gmail.com>
2024-08-10 01:18:52 +0530

    portfolio

--- pages/Projects.js
+++ pages/Projects.js
@@ -19,7 +19,7 @@ export default function Projects() {
       <Grid container spacing={4}>
         <Grid item xs={6} md={4}>
           <Item className="animate__animated animate__fadeIn animate__delay-1s">
-           <div>
+           <div className='w-fit'>
             <h1 className='font-bold'>project-1</h1>
             <h1>food App</h1>
             <span>TechStack:html,css,javascript,node</span>
